73d1896814535bfe33ff6acb8e72a5ed01846374
Yogesh Prabhu <yogeshprabhu@hotmail.com>
2021-10-24 23:37:42 +0000

    Use dotnet cli to create console app, add .gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SportsStatsTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
