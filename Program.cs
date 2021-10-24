0a853957f315f13f88ee0c8ea4cea21e5e85948a
Yogesh Prabhu <yogeshprabhu@hotmail.com>
2021-10-24 23:52:39 +0000

    Add support for the Redis cache

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.Extensions.Configuration;
- using System.IO;
+using System.IO;
+using StackExchange.Redis;
 
 namespace SportsStatsTracker
 {
@@ -14,6 +15,24 @@ namespace SportsStatsTracker
                 .Build();
 
             string connectionString = config["CacheConnection"];
+
+            // The connection to Azure Cache for Redis is managed by the ConnectionMultiplexer class. 
+            // This class should be shared and reused throughout your client application. We do not 
+            // want to create a new connection for each operation. Here, we are only going to use it 
+            // in the Main method, but in a production application, it should be stored in a class 
+            // field, or a singleton.
+            using (var cache = ConnectionMultiplexer.Connect(connectionString))
+            {
+                IDatabase db = cache.GetDatabase();
+                
+                // Add a value to the cache
+                bool setValue = db.StringSet("test:key", "some value");
+                Console.WriteLine($"SET: {setValue}");
+
+                // Get a value from the cache
+                string getValue = db.StringGet("test:key");
+                Console.WriteLine($"GET: {getValue}");
+            }
         }
     }
 }
